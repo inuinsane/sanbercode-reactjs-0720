2ff17e7ad31b82c4a08f61d78500da6aad1253fb
Abdullah Ibnu Hasan <38773546+inuinsane@users.noreply.github.com>
2020-07-24 18:06:24 +0800

    Day 5 - Javascript Function

---
+++ Pekan 1/Tugas Hari ke-5/tugas5.js
@@ -0,0 +1,50 @@
+// Jawaban Soal 1
+console.log(`\n------ dibawah ini adalah jawaban soal pertama ------`);
+
+function halo() {
+    return `"Halo Sanbers!"`;
+}
+console.log(halo());
+console.log('\n');
+
+
+
+
+
+
+
+// Jawaban Soal 2
+console.log(`\n------ dibawah ini adalah jawaban soal kedua ------`);
+
+function kalikan (a, b) {
+    return a*b;
+}
+
+var num1 = 12
+var num2 = 4
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali)
+
+
+
+
+
+
+
+// Jawaban Soal 3
+console.log(`\n------ dibawah ini adalah jawaban soal ketiga ------`);
+
+function introduce(name, age, address, hobby) {
+    return `"Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}!"`;
+}
+
+var name = "John"
+var age = 30
+var address = "Jalan belum jadi"
+var hobby = "Gaming"
+ 
+var perkenalan = introduce(name, age, address, hobby)
+console.log(perkenalan)
+
+
+console.log('\n');
