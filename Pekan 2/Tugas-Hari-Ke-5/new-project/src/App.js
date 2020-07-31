b1ebff0f2b7100ee78a65c7a0518570b81003474
Abdullah Ibnu Hasan <38773546+inuinsane@users.noreply.github.com>
2020-07-31 18:53:19 +0800

    Week 2 - Day 5 (ReactJS introduction)

--- Pekan 2/Tugas-Hari-Ke-5/new-project/src/App.js
+++ Pekan 2/Tugas-Hari-Ke-5/new-project/src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Form Pembelian Buah</h1>
+      <div className="table">
+        <table>
+          <tr>
+            <td className="left column1 mb-5">Nama Pelanggan</td>
+            <td>
+              <input type="text" className="left mb-10"/>
+            </td>
+          </tr>
+          <tr>
+            <td className="left bottom">Daftar Item</td>
+            <td className="left">
+              <input type="checkbox" className="left" /> Semangka
+              <div className="break"></div>
+              <input type="checkbox" className="left" /> Jeruk
+              <div className="break"></div>
+              <input type="checkbox" className="left" /> Nanas
+              <div className="break"></div>
+              <input type="checkbox" className="left" /> Salak
+              <div className="break"></div>
+              <input type="checkbox" className="left" /> Anggur
+            </td>
+          </tr>
+          <tr>
+            <td className="left">
+              <button className="btn">Kirim</button>
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
